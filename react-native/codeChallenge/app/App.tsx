2c05ffdcd0a691235eaa5661969c4e4bfcc98422
Telmo J Moura <telmop1999@gmail.com>
2025-03-28 16:22:02 +0000

    feat: initial Config

---
+++ react-native/codeChallenge/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SafeAreaView, View, Text } from 'react-native'
+
+export default function RootApp() {
+
+    return (
+        <SafeAreaView>
+            <View>
+                <Text> Hello World App</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
