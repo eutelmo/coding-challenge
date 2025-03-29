889cf2a30e69883d2266304b342c62cc2f7da553
Telmo J Moura <telmop1999@gmail.com>
2025-03-29 15:09:15 +0000

    quickFix: Safe hook for modal

--- react-native/codeChallenge/screens/dashboardScreen/dashboardScreen.tsx
+++ react-native/codeChallenge/screens/dashboardScreen/dashboardScreen.tsx
@@ -1,26 +1,45 @@
-import React, { useContext } from 'react'
-import { SafeAreaView, View, Text, TouchableOpacity, RefreshControl, Modal } from 'react-native'
+import React from 'react'
+import { SafeAreaView, View, RefreshControl, Modal } from 'react-native'
 
 //Styles
 import { styles } from './styles';
+
+//Utils
+import { useTransaction } from '@/utils/hooks/useTransaction';
+import { ScrollView } from 'react-native-gesture-handler';
+import { useSnackBarContext } from '@/utils/hooks/useSnackBar';
+import { useModalContext } from '@/utils/hooks/useModal';
 
 //Components
 import Summary from '@/components/shared/Summary/SummaryComp';
 import TransactionList from '@/components/shared/TrasactionList/TransactionListComp';
 import CarousselComp from '@/components/Caroussel/CarousselComp';
 import ModalComponent from '@/components/shared/Modal';
-
-//Utils
-import { useTransaction } from '@/utils/hooks/useTransaction';
-import { ScrollView } from 'react-native-gesture-handler';
-import { ModalContext } from '@/context/modelContext';
+import SnackBarComponent from '@/components/shared/SnackBars';
 
 export default function DashboardScreen({ navigation }: any) {
     const { isLoading, refetch } = useTransaction();
     const {
         isVisible,
         handleCloseModal,
-    } = useContext(ModalContext);
+    } = useModalContext();;
+
+    const {
+        isErrorGetProducts,
+        closeSnackBarGetProducts,
+
+        isErrorGetTransactions,
+        closeSnackBarGetTransactions,
+
+        isErrorOpenModal,
+        closeSnackBarOpenModal,
+
+        isErrorInternetConnection,
+        closeSnackBarInternetConnection,
+
+        isErrorGeneral,
+        closeSnackBarGeneral,
+    } = useSnackBarContext();
 
 
     return (
@@ -37,6 +56,42 @@ export default function DashboardScreen({ navigation }: any) {
             >
                 <ModalComponent />
             </Modal>
+
+            {/* SnackBar for get products error */}
+            <SnackBarComponent
+                message="Error fetching products!"
+                isVisible={isErrorGetProducts}
+                closeFunction={closeSnackBarGetProducts}
+            />
+
+            {/* SnackBar for get transactions error */}
+            <SnackBarComponent
+                message="Error fetching transactions!"
+                isVisible={isErrorGetTransactions}
+                closeFunction={closeSnackBarGetTransactions}
+            />
+
+            {/* SnackBar for open modal error */}
+            <SnackBarComponent
+                message="Error opening modal!"
+                isVisible={isErrorOpenModal}
+                closeFunction={closeSnackBarOpenModal}
+            />
+
+            {/* SnackBar for internet connection error */}
+            <SnackBarComponent
+                message="No internet connection!"
+                isVisible={isErrorInternetConnection}
+                closeFunction={closeSnackBarInternetConnection}
+            />
+
+            {/* SnackBar for general error */}
+            <SnackBarComponent
+                message="An unexpected error occurred!"
+                isVisible={isErrorGeneral}
+                closeFunction={closeSnackBarGeneral}
+            />
+
             <SafeAreaView style={styles.container}>
                 <ScrollView
                     contentContainerStyle={{ flexGrow: 1 }}
